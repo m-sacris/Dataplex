6b9f6f746298434a88bc720751bd9a489b6764f5
Marc Sacristan Castillo <masacris@emeal.nttdata.com>
2026-03-11 11:12:17 +0100

    iamPolices

--- google_dataplex_lake/main.tf
+++ google_dataplex_lake/main.tf
@@ -9,3 +9,18 @@ resource "google_dataplex_lake" "primary" {
     my-lake = "exists"
   }
 }
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/Dataplex Service Agent"
+    members = [
+      "user:masacris@emeal.nttdata.com",
+    ]
+  }
+}
+
+resource "google_dataplex_lake_iam_policy" "policy" {
+  project = google_dataplex_lake.primary.project
+  location = google_dataplex_lake.primary.location
+  lake = google_dataplex_lake.primary.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
